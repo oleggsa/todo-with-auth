fe1856d1c5acc62400366936e0dc95fb6e2d1907
Oleh Didyk <braun.ufo@gmail.com>
2022-06-03 22:34:02 +0300

    animations on modal

--- src/components/ShowTodoForm.vue
+++ src/components/ShowTodoForm.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="show-todo" v-if="show" @click="hideDialog">
-    <div @click.stop class="show-todo__inner">
-      <div class="header">
-        <span>{{targetTodo.title}}</span>
-        <i class="fa-solid fa-xmark" @click="hideDialog"></i>
-      </div>
-      <div class="description">
-        {{targetTodo.description}}
-      </div>
+  <transition name="fade" appear>
+    <div class="show-todo" v-if="show" @click="hideDialog">
+      <transition name="pop" appear>
+        <div @click.stop class="show-todo__inner">
+          <div class="header">
+            <span>{{targetTodo.title}}</span>
+            <i class="fa-solid fa-xmark" @click="hideDialog"></i>
+          </div>
+          <div class="description">
+            {{targetTodo.description}}
+          </div>
+        </div>
+      </transition>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class="dialog" v-if="show" @click="hideDialog">
-    <div @click.stop class="dialog-inner">
-      <div class="todo-create">
-        <form @submit.prevent>
-          <h4>{{ !isEditing ? 'Create' : 'Edit' }} TODO</h4>
-          <input
-              @input="newTodo.title = $event.target.value"
-              v-bind:value="isEditing ? newTodo.title = todoToEdit.title : newTodo.title = ''"
-              type="text"
-              placeholder="Title"
-          >
-          <textarea
-              @input="newTodo.description = $event.target.value"
-              v-bind:value="isEditing ? newTodo.description = todoToEdit.description : newTodo.description = ''"
-              type="text"
-              placeholder="Description"
-          ></textarea>
-          <primary-button @click.native="isEditing ? createTodo('update') : createTodo('new')">{{!isEditing ? 'Submit' : 'Edit'}}</primary-button>
-        </form>
-      </div>
+  <transition name="fade" appear>
+    <div class="dialog" v-if="show" @click="hideDialog">
+      <transition name="pop" appear>
+        <div @click.stop class="dialog-inner">
+          <div class="todo-create">
+            <form @submit.prevent>
+              <h4>{{ !isEditing ? 'Create' : 'Edit' }} TODO</h4>
+              <input
+                  @input="newTodo.title = $event.target.value"
+                  v-bind:value="isEditing ? newTodo.title = todoToEdit.title : newTodo.title = ''"
+                  type="text"
+                  placeholder="Title"
+              >
+              <textarea
+                  @input="newTodo.description = $event.target.value"
+                  v-bind:value="isEditing ? newTodo.description = todoToEdit.description : newTodo.description = ''"
+                  type="text"
+                  placeholder="Description"
+              ></textarea>
+              <primary-button @click.native="isEditing ? createTodo('update') : createTodo('new')">{{!isEditing ? 'Submit' : 'Edit'}}</primary-button>
+            </form>
+          </div>
+        </div>
+      </transition>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
